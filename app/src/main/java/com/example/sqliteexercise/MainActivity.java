520b2340d45705aa8f2fbcb8c60228346dd42fee
Muhammad Ferdy Yosa <ferdyzecter@gmail.com>
2020-04-21 05:59:01 +0700

    Menambah activity dan layout, serta mengedit-ngedit banyak isi projek

--- app/src/main/java/com/example/sqliteexercise/MainActivity.java
+++ app/src/main/java/com/example/sqliteexercise/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.sqliteexercise;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -20,47 +24,77 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "MESSAGE";
-    private ListView obj;
-    DBHelper mydb;
+    String[] daftar;
+    ListView ListView01;
+    Menu menu;
+    protected Cursor cursor;
+    DataHelper dbcenter;
+    public static MainActivity ma;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        mydb = new DBHelper(this);
-        ArrayList array_list = mydb.getAllCotacts();
-        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1, array_list);
-        obj = (ListView)findViewById(R.id.listView1);
-        obj.setAdapter(arrayAdapter);
-        obj.setOnItemClickListener(new
-                                           AdapterView.OnItemClickListener(){
-                                               @Override
-                                               public void onItemClick(AdapterView<?> arg0, View arg1, int
-                                                       arg2, long arg3) {
-                                                   // TODO Auto-generated method stub
-                                                   int id_To_Search = arg2 + 1;
-                                                   Bundle dataBundle = new Bundle();
-                                                   dataBundle.putInt("id", id_To_Search);
-                                                   Intent intent = new
-                                                           Intent(getApplicationContext(),DisplayMhs.class);
-                                                   intent.putExtras(dataBundle);
-                                                   startActivity(intent);
-                                               }
-                                           });
         FloatingActionButton fab = findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new
-                        Intent(getApplicationContext(), DisplayMhs.class);
+                Intent intent = new Intent(MainActivity.this, BuatBiodataActivity.class);
                 startActivity(intent);
             }
 
         });
+        ma = this;
+        dbcenter = new DataHelper((this));
+        RefreshList();
     }
 
+    public void RefreshList() {
+        SQLiteDatabase db = dbcenter.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM biodata", null);
+        daftar = new String[cursor.getCount()];
+        cursor.moveToFirst();
+        for (int cc = 0; cc < cursor.getCount(); cc++) {
+            cursor.moveToPosition(cc);
+            daftar[cc] = cursor.getString(1).toString();
+        }
+        ListView01 = (ListView) findViewById(R.id.listView1);
+        ListView01.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, daftar));
+        ListView01.setSelected(true);
+        ListView01.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView arg0, View arg1, int arg2, long arg3) {
+                final String selection = daftar[arg2];
+                final CharSequence[] dialogitem = {"Lihat Biodata", "Update Biodata", "Hapus Biodata"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Pilihan");
+                builder.setItems(dialogitem, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int item) {
+                        switch(item) {
+                            case 0:
+                                Intent i1 = new Intent(getApplicationContext(), LihatBiodataActivity.class);
+                                i1.putExtra("nama", selection);
+                                startActivity(i1);
+                                break;
+                            case 1:
+                                Intent i2 = new Intent(getApplicationContext(), UpdateBiodataActivity.class);
+                                i2.putExtra("nama", selection);
+                                startActivity(i2);
+                                break;
+                            case 2:
+                                SQLiteDatabase db = dbcenter.getWritableDatabase();
+                                db.execSQL("delete from biodata where nama = '" + selection + "'");
+                                RefreshList();
+                                break;
+                        }
+                    }
+                });
+                builder.create().show();
+            }
+        });
+        ((ArrayAdapter) ListView01.getAdapter()).notifyDataSetInvalidated();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -68,25 +102,13 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        super.onOptionsItemSelected(item);
-        switch(item.getItemId()) {
-            case R.id.item1:Bundle dataBundle = new Bundle();
-                dataBundle.putInt("id", 0);
-                Intent intent = new
-                        Intent(getApplicationContext(),DisplayMhs.class);
-                intent.putExtras(dataBundle);
-                startActivity(intent);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-    public boolean onKeyDown(int keycode, KeyEvent event) {
-        if (keycode == KeyEvent.KEYCODE_BACK) {
-            moveTaskToBack(true);
-        }
-        return super.onKeyDown(keycode, event);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item){
+//        int id = item.getItemId();
+//        if(id == R.id.action_settings){
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+
 }
