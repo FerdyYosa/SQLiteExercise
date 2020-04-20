9c46b6559a93655fc993a76b826d6b5068e2b636
Muhammad Ferdy Yosa <ferdyzecter@gmail.com>
2020-04-20 17:40:56 +0700

    Menambah kode mengedit fungsi pada FAB

--- app/src/main/java/com/example/sqliteexercise/MainActivity.java
+++ app/src/main/java/com/example/sqliteexercise/MainActivity.java
@@ -53,8 +53,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new
+                        Intent(getApplicationContext(), DisplayMhs.class);
+                startActivity(intent);
             }
 
         });
