63e795b8ccfb2f96d8d6a3245c9c51775b32e5ab
Muhammad Ferdy Yosa <ferdyzecter@gmail.com>
2020-04-20 16:23:14 +0700

    Membuat seperti projek terbaru dan menambah FAB

---
+++ app/src/main/java/com/example/sqliteexercise/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.sqliteexercise;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "MESSAGE";
+    private ListView obj;
+    DBHelper mydb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mydb = new DBHelper(this);
+        ArrayList array_list = mydb.getAllCotacts();
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1, array_list);
+        obj = (ListView)findViewById(R.id.listView1);
+        obj.setAdapter(arrayAdapter);
+        obj.setOnItemClickListener(new
+                                           AdapterView.OnItemClickListener(){
+                                               @Override
+                                               public void onItemClick(AdapterView<?> arg0, View arg1, int
+                                                       arg2, long arg3) {
+                                                   // TODO Auto-generated method stub
+                                                   int id_To_Search = arg2 + 1;
+                                                   Bundle dataBundle = new Bundle();
+                                                   dataBundle.putInt("id", id_To_Search);
+                                                   Intent intent = new
+                                                           Intent(getApplicationContext(),DisplayMhs.class);
+                                                   intent.putExtras(dataBundle);
+                                                   startActivity(intent);
+                                               }
+                                           });
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        super.onOptionsItemSelected(item);
+        switch(item.getItemId()) {
+            case R.id.item1:Bundle dataBundle = new Bundle();
+                dataBundle.putInt("id", 0);
+                Intent intent = new
+                        Intent(getApplicationContext(),DisplayMhs.class);
+                intent.putExtras(dataBundle);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    public boolean onKeyDown(int keycode, KeyEvent event) {
+        if (keycode == KeyEvent.KEYCODE_BACK) {
+            moveTaskToBack(true);
+        }
+        return super.onKeyDown(keycode, event);
+    }
+}
